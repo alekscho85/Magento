4a30b43c7163c7364b9af26be7e2ecc82994455b
Aleksandar <aleksdimitrov85@gmail.com>
2017-06-13 22:18:49 +0300

    Statistics template&helper

---
+++ app/code/local/SoftUni/Statistics/Helper/Data.php
@@ -0,0 +1,8 @@
+<?php
+class SoftUni_Statistics_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    public function isSoftuniStatisticsOrdersEnabled()
+    {
+        return Mage::getStoreConfigFlag('general/softuni_statistics_orders/statistics_orders_enabled');
+    }
+}
